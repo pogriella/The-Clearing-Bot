e32c99949db69a6c69757da52abe0d3121601906
Garrett <gevansimpson@gmail.com>
2022-02-08 09:38:36 -0500

    Deleted upstream changes in files

--- commands/clear.js
+++ commands/clear.js
@@ -1,29 +1,3 @@
-<<<<<<< Updated upstream
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { clearingChannelIds } = require('../config.json');
-
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('clearing')
-        .setDescription('Purges the server.'),
-    async execute(interaction) {
-        const del = await interaction.guild.channels.cache.filter(channel => channel.isText())
-            .filter(channel => clearingChannelIds.includes(channel.id))
-            .forEach(async channel => {
-                const textChannel = channel.guild.channels.cache.get(channel.id);
-                for (let i = 0; i < 10; i++) await textChannel.bulkDelete(100, true);
-            });
-        
-        await eval(del);
-        await interaction.editReply('Done.').then(() => {
-            interaction.guild.channels.cache.filter(channel => channel.isText())
-                .filter(channel => clearingChannelIds.includes(channel.id))
-                .forEach(channel => {
-                    channel.guild.channels.cache.get(channel.id).send('https://tenor.com/view/maid-clapping-singing-anime-gif-8700993');
-                });
-        });
-    }
-=======
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { clearingChannelIds } = require('../config.json');
 
@@ -56,5 +30,4 @@ module.exports = {
                 });
         });
     }
->>>>>>> Stashed changes
 }
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 require('dotenv').config();
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
@@ -34,47 +33,4 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-client.on('messageCreate', async message => {
-    if (!message.content.startsWith(prefix)) return;
-
-    const messageCommand = message.content.slice(prefix.length).split(/ +/).join(' ').split(' ')[0];
-});
-
-=======
-require('dotenv').config();
-const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
-const { prefix } = require('./config.json');
-
-const client = new Client({ intents: [ Intents.FLAGS.GUILDS ]});
-
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
-}
- 
-client.once('ready', () => {
-    console.log('On.');
-    client.user.setPresence({ activities: [{ name: 'with circles.', type: 'PLAYING' }], status: 'online' });
-});
-
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await interaction.deferReply();
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-    }
-});
-
->>>>>>> Stashed changes
 client.login(process.env.TOKEN);
